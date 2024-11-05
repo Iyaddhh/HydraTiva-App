a5615b874c4aa5ef8f248e3612bab2111437580b
Riyadh Azhara <riyadhazhara82@gmail.com>
2024-11-05 13:18:57 +0700

    Revert ""Watering Terbaru""
    This reverts commit 0e6c80dfcb29bce1b7e8c71bc5ad93904a87ba31.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,7 +36,6 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation("com.github.bumptech.glide:glide:4.16.0")
-    implementation("com.github.zerobranch:SwipeLayout:1.3.1")
     implementation(libs.constraintlayout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
