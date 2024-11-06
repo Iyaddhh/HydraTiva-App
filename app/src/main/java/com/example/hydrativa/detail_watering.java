1b680e380619cb84040fba541f56a4adfe6b4c0c
Takulian <miftakhulghulam10@gmail.com>
2024-11-06 13:23:41 +0700

    fix detail kebun

--- app/src/main/java/com/example/hydrativa/detail_watering.java
+++ app/src/main/java/com/example/hydrativa/detail_watering.java
@@ -13,7 +13,7 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import com.example.hydrativa.models.DetailKebun;
+
 import com.example.hydrativa.models.DetailKebunResponse;
 import com.example.hydrativa.models.Kebun;
 import com.example.hydrativa.models.KebunResponse;
@@ -31,7 +31,7 @@ import retrofit2.Response;
 
 public class detail_watering extends AppCompatActivity {
 
-    private TextView tvMoisture, tvPH, tvStatus, tvTanggalPenyiraman;
+    private TextView tvMoisture, tvPH, tvStatus, tvTanggal, tvTitle, tvLocation;
     private KebunService kebunService;
     private Kebun kebunDetail;
     private int kebunId;
@@ -41,10 +41,14 @@ public class detail_watering extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_watering);
 
+        kebunService = RetrofitClient.getRetrofitInstance(getApplicationContext()).create(KebunService.class);
+
+        tvTitle = findViewById(R.id.Title);
+        tvLocation = findViewById(R.id.Location);
         tvMoisture = findViewById(R.id.Moisture);
         tvPH = findViewById(R.id.PH);
         tvStatus = findViewById(R.id.Status);
-        tvTanggalPenyiraman = findViewById(R.id.TanggalKebun);
+        tvTanggal = findViewById(R.id.TanggalKebun);
 
         Date date = new Date();
         SimpleDateFormat formatter = new SimpleDateFormat("EEE, d MMM yyyy");
@@ -59,11 +63,13 @@ public class detail_watering extends AppCompatActivity {
                 if (response.isSuccessful() && response.body() != null) {
                     kebunDetail = response.body();
 
-                    // Menampilkan data alat di TextView
+                    Log.d("WKWK", "bang: " + response);
+                    tvTitle.setText(kebunDetail.getNama_kebun());
+                    tvLocation.setText(kebunDetail.getLokasi_kebun());
                     tvMoisture.setText("Moisture: " + kebunDetail.getMoisture());
-                    tvPH.setText("pH: " + kebunDetail.getpH());
+                    tvPH.setText(kebunDetail.getpH() + " pH");
                     tvStatus.setText("Status: " + kebunDetail.getStatus());
-                    tvTanggalPenyiraman.setText("Tanggal Penyiraman: " + currentDate);
+                    tvTanggal.setText(currentDate);
                 } else {
                     Toast.makeText(detail_watering.this, "Gagal memuat data alat", Toast.LENGTH_SHORT).show();
                 }
@@ -76,47 +82,4 @@ public class detail_watering extends AppCompatActivity {
             }
         });
     }
-
-//    private void getKebunDetail(int kebunId) {
-//        Call<Kebun> call = kebunService.getKebunDetail(kebunId);
-//        call.enqueue(new Callback<Kebun>() {
-//            @Override
-//            public void onResponse(Call<Kebun> call, Response<Kebun> response) {
-//                if (response.isSuccessful() && response.body() != null) {
-//                    kebunDetail = response.body();
-//                    tvMoisture.setText("Moisture: " + kebunDetail.getMoisture());
-//                    tvPH.setText("pH: " + kebunDetail.getPh());
-//                    tvStatus.setText("Status: " + kebunDetail.getStatus());
-//                    tvTanggalPenyiraman.setText("Tanggal Penyiraman: " + kebunDetail.getTanggalPenyiraman());
-//                } else {
-//                    Toast.makeText(detail_watering.this, "Gagal memuat data alat", Toast.LENGTH_SHORT).show();
-//                }
-//            }
-//
-//            @Override
-//            public void onFailure(Call<Kebun> call, Throwable throwable) {
-//
-//            }
-
-//            @Override
-//            public void onResponse(Call<DetailKebunResponse> call, Response<DetailKebunResponse> response) {
-//                if (response.isSuccessful() && response.body() != null) {
-//                    kebunDetail = response.body().getKebun();
-//
-//                    // Menampilkan data alat di TextView
-//                    tvMoisture.setText("Moisture: " + kebunDetail.getMoisture());
-//                    tvPH.setText("pH: " + kebunDetail.getPh());
-//                    tvStatus.setText("Status: " + kebunDetail.getStatus());
-//                    tvTanggalPenyiraman.setText("Tanggal Penyiraman: " + kebunDetail.getTanggalPenyiraman());
-//                } else {
-//                    Toast.makeText(detail_watering.this, "Gagal memuat data alat", Toast.LENGTH_SHORT).show();
-//                }
-//            }
-//
-//            @Override
-//            public void onFailure(Call<DetailKebunResponse> call, Throwable t) {
-//                Toast.makeText(detail_watering.this, "Error: " + t.getMessage(), Toast.LENGTH_SHORT).show();
-//            }
-//        });
-//    }
 }
